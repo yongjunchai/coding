3f14753d8257ad25cd90fe9357e1413b0b3e8d70
Yongjun Chai <yongjunchai@hotmail.com>
2022-05-09 21:32:11 -0700

    v2 of longest command substring

--- src/test/java/LongestCommonSubstringTest.java
+++ src/test/java/LongestCommonSubstringTest.java
@@ -20,14 +20,19 @@ public class LongestCommonSubstringTest {
         }
     }
 
-    @Test
-    public void testLcs() {
+    private List<Entry> getTestData() {
         List<Entry> entries = new ArrayList<>();
-        entries.add(Entry.create("helloabc", "chhhelloadef", "helloa"));
+        entries.add(Entry.create("d helloa f ld", "ppp chhhelloadef k ld helloa f ld", "d helloa f ld"));
         entries.add(Entry.create("", "chhhelloadef", ""));
         entries.add(Entry.create("abc", "def", ""));
         entries.add(Entry.create("hi how are you.", "i am fine, and you.", " you."));
         entries.add(Entry.create("hi how are you. good", "i am fine, and you. see you. bye.", "e you. "));
+        return entries;
+    }
+
+    @Test
+    public void testLcs() {
+        List<Entry> entries = getTestData();
         LongestCommonSubstring longestCommonSubstring = new LongestCommonSubstring();
         Utility.FetchValue<LongestCommonSubstring.Note> fetchValue = (LongestCommonSubstring.Note a) -> {return Integer.toString(a.substringLen);} ;
         for (Entry entry: entries) {
@@ -37,6 +42,21 @@ public class LongestCommonSubstringTest {
             System.out.println(result.commonSubstring);
             Utility.dump(result.notes, fetchValue);
             Assert.assertTrue(entry.lcs.equals(result.commonSubstring));
+        }
+    }
+
+    @Test
+    public void testLcsV2() {
+        List<Entry> entries = getTestData();
+        LongestCommonSubstringV2 longestCommonSubstringV2 = new LongestCommonSubstringV2();
+        Utility.FetchValue<Integer> fetchValue = (a) -> {return Integer.toString(a);} ;
+        for (Entry entry: entries) {
+            LongestCommonSubstringV2.Result result = longestCommonSubstringV2.longestCommonSubstring(entry.str1.toCharArray(), entry.str2.toCharArray());
+            System.out.println(entry.str1);
+            System.out.println(entry.str2);
+            System.out.println(result.commonString);
+            Utility.dump(result.subProblems, fetchValue);
+            Assert.assertTrue(entry.lcs.equals(result.commonString));
         }
     }
 }
